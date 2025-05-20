eed0c646b880ded3f84019df29d2d9f09f90d5cf
Tam <tamknguyen3@gmail.com>
2025-05-20 14:07:37 -0400

    header, references

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+OPI Data Engineer – Exercise
+Received on May 5th, 2025 @ 10am est
+Due on May 7th, 2025 @ 10am est
+Tam Nguyen
+tamnguyencs25@gmail.com 
+"""
+
+# ------ REFERENCES ------
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.html
+# https://geopandas.org/en/stable/docs/user_guide/io.html
+# ------ EXPLORING TAX & PROPERTY DATASETS (CSV & GEOJSON) ------
